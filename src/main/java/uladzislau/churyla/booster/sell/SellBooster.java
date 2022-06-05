b77b136d15e0b55a9dc3f601a08358002d5208ef
Uladzislau Churyla <Uladzislau_Churyla@epam.com>
2022-06-05 13:59:16 +0300

    add autosell

--- src/main/java/uladzislau/churyla/booster/sell/SellBooster.java
+++ src/main/java/uladzislau/churyla/booster/sell/SellBooster.java
@@ -1,32 +1,37 @@
 package uladzislau.churyla.booster.sell;
 
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.commands.Commandsell;
 import net.ess3.api.events.UserBalanceUpdateEvent;
 import net.ess3.api.events.UserBalanceUpdateEvent.Cause;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
-import net.luckperms.api.node.NodeType;
-import net.luckperms.api.node.types.PermissionNode;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class SellBooster extends JavaPlugin implements Listener {
 
     private final Pattern permissionPattern = Pattern.compile("^boost.money.[1-9][0-9]{0,4}$");
+    private LuckPerms luckPerms;
+
+    private Essentials essentials;
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
         Utils.cfg = this.getConfig();
+        luckPerms = LuckPermsProvider.get();
+        essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
@@ -64,6 +69,20 @@ public class SellBooster extends JavaPlugin implements Listener {
 
         e.setNewBalance(e.getNewBalance().add(cashback));
         player.sendMessage(Utils.formatMSG("cashback-success", cashback.doubleValue()));
+    }
+
+    @EventHandler
+    public void onPlayerPickUpItemEventHandler(EntityPickupItemEvent e) {
+        Commandsell commandsell = new Commandsell();
+        final var entity = e.getEntity();
+        if (!(entity instanceof Player)) return;
+
+        var player = (Player) entity;
+        try {
+            commandsell.run(Bukkit.getServer(), essentials.getUser(player), "sell", new String[]{"all"});
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
 }
